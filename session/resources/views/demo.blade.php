3edc2309907bb514449cb5cc8f721cadd15a040f
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2021-05-18 09:56:21 +0600

    some changed

---
+++ session/resources/views/demo.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Demo Practice</title>
+</head>
+<body>
+    <h1>This is a nice T-shirt</h1>
+    <h1>The Weather is so hot today!!! shit man!!</h1>
+</body>
+</html>
